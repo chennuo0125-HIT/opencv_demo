67cefafb8e2a9a5a949842483be2628aa1c15f78
chennuo <chennuo0125@163.com>
2021-01-07 18:28:20 +0800

    add support for use_cam, fix opencv yaml object cant read bool type bug

--- aruco/src/estimate_pose_demo.cpp
+++ aruco/src/estimate_pose_demo.cpp
@@ -1,6 +1,8 @@
 #include "aruco.h"
 #include <opencv2/core.hpp>
 
+#define VIEW_ONLINE false
+
 using namespace std;
 
 int main (int argc, char** argv)
@@ -8,14 +10,6 @@ int main (int argc, char** argv)
     if (argc != 4)
     {
         cout << "please run: ./node dict_name param.yaml img !!!" << endl;
-        return 0;
-    }
-
-    // read detecting image
-    cv::Mat image = cv::imread(argv[3]).clone();
-    if (image.empty())
-    {
-        cout << "image is empty !!!" << endl;
         return 0;
     }
 
@@ -40,6 +34,40 @@ int main (int argc, char** argv)
     
     // config aruco for estimate pose
     aruco.configCamAndPrintedMarker(cam_mat, dist_coef, side_length, use_fisheye);
+#if VIEW_ONLINE
+    cv::VideoCapture vcap(0);
+    cv::Mat image;
+    cv::namedWindow("result", CV_WINDOW_NORMAL);
+    while (cv::waitKey(100) != 27)
+    {
+        vcap >> image;
+        if (image.empty()) break;
+
+        // estimate pose
+        vector<cv::Vec3d> rvecs, tvecs;
+        aruco.estimateMarkerPose(image, rvecs, tvecs);
+        assert(rvecs.size() == tvecs.size());
+        // for (int i = 0; i < rvecs.size(); i++)
+        // {
+        //     cout << "marker " << i << " : rvec->[" << rvecs[i][0] << "," << rvecs[i][1] << "," << rvecs[i][2] 
+        //         << "], tvec->[" << tvecs[i][0] << "," << tvecs[i][1] << "," << tvecs[i][2] << "]" << endl;
+        // }
+        // cout << endl;
+
+        // view result image
+        cv::Mat copy_img = image.clone();
+        aruco.drawMarkerAxis(copy_img);
+
+        cv::imshow("result", copy_img);
+    }
+#else
+    // read detecting image
+    cv::Mat image = cv::imread(argv[3]).clone();
+    if (image.empty())
+    {
+        cout << "image is empty !!!" << endl;
+        return 0;
+    }
 
     // estimate pose
     vector<cv::Vec3d> rvecs, tvecs;
@@ -55,6 +83,7 @@ int main (int argc, char** argv)
     cv::Mat copy_img = image.clone();
     aruco.drawMarkerAxis(copy_img);
     cv::imwrite("result.jpg", copy_img);
+#endif
 
     return 0;
 }
